ca42c6b04d913066818c773a736793131d62d098
Karmester <ptrarmand10@gmail.com>
2022-02-16 19:26:22 +0100

    fetch + search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,70 @@
-const Search = () => {
-    return (
-        <div>Search</div>
-    )
+import { useState, useEffect } from "react";
+import { Navbar, Container } from "react-bootstrap";
+
+import { search } from "./api";
+import SearchPic from "./SearchPic";
+import SearchResults from "./SearchResults";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Search.css";
+
+function Search() {
+	const [results, setResults] = useState(null);
+	const [query, setQuery] = useState("");
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		// Clear the results if the user gets rid of the search query
+		if (!(query || query.length)) {
+			setResults(null);
+			return;
+		}
+
+		// Don't bother searching for anything less than 3 characters
+		if (query.length < 3) {
+			return;
+		}
+
+		// Otherwise, start a search
+		setLoading(true);
+		search(query, "id", "title", "image_id", "thumbnail")
+			.then((searchResults) => {
+				if (searchResults && searchResults.data) {
+					setResults(searchResults.data);
+				}
+			})
+			.catch((err) => setError(err))
+			.finally(() => setLoading(false));
+	}, [query]);
+
+	return (
+		<div className="Search">
+			<header>
+				{/* <Navbar bg="dark" variant="dark">
+					<Navbar.Brand>Atrsy</Navbar.Brand>
+				</Navbar> */}
+				<div fluid className="painting-background Jumbotron">
+					<Container className="text-center">
+						<h1>Find Art You Love</h1>
+						<SearchPic
+							query={query}
+							onChange={(e) => setQuery(e.target.value)}
+						/>
+					</Container>
+				</div>
+			</header>
+			<main>
+				<Container fluid>
+					{error ? (
+						<p>Unable to retrieve results.</p>
+					) : (
+						<SearchResults results={results} loading={loading} />
+					)}
+				</Container>
+			</main>
+		</div>
+	);
 }
 
-export default Search
+export default Search;
