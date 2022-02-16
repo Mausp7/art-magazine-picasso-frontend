4996d09a1aa27cd2afd80475834ebd450baaf507
Mausp7 <archie.tombacz@gmail.com>
2022-02-16 09:48:48 +0100

    Adds React Router and structure.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import Collection from "./components/Collection";
+
 const App = () => {
 	return (
-		<div className="App">
-			<h1>Hello Picasso!</h1>
-		</div>
+		<Router>
+			<Header />
+			<Routes>
+				<Route path="/" exact element={<Search />} />
+				<Route path="/collection" element={<Collection />} />		    
+			</Routes>
+		</Router>
 	);
 }
 
