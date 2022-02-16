90503c4436a012ea424c28af5990d5d173a5b317
agolautner <lautner.agoston@gmail.com>
2022-02-16 11:54:16 +0100

    render card components with dummy data

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from 'react';
 
-const Card = () => {
+const lorem = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, provident architecto similique voluptatem qui eaque perferendis nulla, consectetur esse omnis enim voluptatibus corporis vel beatae nobis id voluptate deleniti error!';
+const src = 'https://static01.nyt.com/images/2018/03/02/arts/design/02picasso-print/01picasso1-superJumbo.jpg';
+
+const Card = ({title = 'Lorem Ipsum', description = lorem, source=src}) => {
   return (
     <div className='Card'>
         <div className='Card-text'>
-            <h2 className='Card-heading'>Lorem ipsum</h2>
-            <p className='Card-description'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, provident architecto similique voluptatem qui eaque perferendis nulla, consectetur esse omnis enim voluptatibus corporis vel beatae nobis id voluptate deleniti error!</p>
+            <h2 className='Card-heading'>{title}</h2>
+            <p className='Card-description'>{description}</p>
         </div>
-        <img className="Card-img" src="https://static01.nyt.com/images/2018/03/02/arts/design/02picasso-print/01picasso1-superJumbo.jpg" alt="Lorem ipsum" />
+        <img className="Card-img" src={source} alt={title} />
     </div>
   )
 }
--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -1,9 +1,31 @@
-import Card from "./Card"
+import Card from "./Card";
+import './Card.css';
+
+const dummyData = [
+    {
+        title: 'Painting One'
+    },
+    {
+        title: 'Painting Two'
+    },
+    {
+        title: 'Painting Three'
+    },
+    {
+        title: 'Painting Four'
+    },
+    {
+        title: 'Painting Five'
+    },
+]
 
 const Collection = () => {
     return (
         <div>
-            <Card />
+            {dummyData.map((p) => {
+                return <Card title={p.title}/>
+            })}
+            {/* <Card /> */}
         </div>
     )
 }
