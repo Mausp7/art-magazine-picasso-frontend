84b651e99b78c02972a51ce1fb3ec13a698d2df0
kordoran <andorkorcsog@gmail.com>
2022-02-21 18:25:06 +0100

    register andor

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,49 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Register.css";
+import http from "axios";
 
 const Register = () => {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signUp = async () => {
+    try {
+      const response = await http.post(
+        "http://localhost:5000/api/user/signup",
+        {
+          username: username,
+          password: password,
+        }
+      );
+      setUsername("");
+      setPassword("");
+      alert("Success");
+    } catch (error) {
+      if (error.response.status === 400) {
+        alert("Missing credentials");
+      } else if (error.response.status === 409)
+        alert("Username already exists");
+    }
+  };
+
   return (
-    <div>Register</div>
+    <div className='card'>
+      <h1>Register</h1>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button className="reg" onClick={(e) => signUp()}>Register</button>
+      <Link to="/login"><button>I already have an account</button></Link>
+    </div>
   )
 }
 
